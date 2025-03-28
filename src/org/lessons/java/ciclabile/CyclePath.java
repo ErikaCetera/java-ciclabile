e8ec581008ee5938eb99d85af12e4f94e4fcc7f6
Erika Cetera <ceteraerika@gmail.com>
2025-03-28 16:25:54 +0100

    feat: metodo add element

--- src/org/lessons/java/ciclabile/CyclePath.java
+++ src/org/lessons/java/ciclabile/CyclePath.java
@@ -1,8 +1,11 @@
 package org.lessons.java.ciclabile;
 
+import java.util.Arrays;
+
 public class CyclePath {
 
-    // inizializzazione di un array di interi e di un intero che rappresenta la posizione
+    // inizializzazione di un array di interi e di un intero che rappresenta la
+    // posizione
     private int[] integer;
     private int currentIndex;
 
@@ -10,6 +13,13 @@ public class CyclePath {
     public CyclePath(int[] integer) {
         this.integer = integer;
         this.currentIndex = 0;
+    }
+
+    // costruttore senza parametri
+    public CyclePath() {
+        this.integer = new int[0];
+        this.currentIndex = 0;
+
     }
 
     // Metodo che esegue il ciclo per stampare tutti gli elementi
@@ -35,6 +45,29 @@ public class CyclePath {
         } else {
             return "Non ci sono altri elementi!";
         }
+    }
+
+    // Metodo per aggiungere un nuovo elemento all'array
+    public void addElem(int element) {
+        // nuovo array con lungezza maggiore
+        int[] newArray = new int[integer.length + 1];
+
+        // copia gli elementi
+        for (int i = 0; i < integer.length; i++) {
+            newArray[i] = integer[i];
+        }
+
+        // aggiunge un nuovo elemento
+        newArray[integer.length] = element;
+
+        // aggiorna l'array iniziale
+        this.integer = newArray;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(integer);
 
     }
+
 }
--- src/org/lessons/java/ciclabile/Main.java
+++ src/org/lessons/java/ciclabile/Main.java
@@ -6,6 +6,8 @@ public static void main(String[] args) {
     int[] integer = {1, 2, 3};
 
  CyclePath Cycle = new CyclePath(integer);
+ CyclePath Cycle1 = new CyclePath();
+ 
    
  
     System.out.println("Elemento: " + Cycle.getNextElem());
@@ -15,6 +17,18 @@ public static void main(String[] args) {
     System.out.println("Elemento: " + Cycle.getNextElem());
     System.out.println("Ci sono ancora elementi? " + Cycle.hasNextElem());
     System.out.println( Cycle.getNextElem());
+    
+    Cycle1.addElem(2);
+    System.out.println(Cycle1);
+    Cycle1.addElem(2);
+    System.out.println(Cycle1);
+    Cycle1.addElem(2);
+    System.out.println(Cycle1);
+
+
+
+
+    
     
    
    
